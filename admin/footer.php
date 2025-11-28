d30fc621a634bc6ee92b74d66e833c1cb8cf26cd
mecca2366-source <mecca2366@gmail.com>
2025-11-28 09:08:57 +0700

    Initial commit

---
+++ admin/footer.php
@@ -0,0 +1,38 @@
+<footer style="background: #111; color: #fff; padding: 50px 0; margin-top: 30px;">
+    <div class="container">
+        <div class="row text-center">
+
+            <!-- Tentang Perusahaan -->
+            <div class="col-md-4" style="padding: 20px;">
+                <h4 style="color: #00aaff; margin-bottom: 15px;">Tentang Kami</h4>
+                <p style="color: #ccc;">
+                    Solusi cepat & bersih untuk semua kebutuhan laundry Anda. Kami siap melayani Anda setiap hari.
+                </p>
+            </div>
+
+            <!-- Pencarian & Kontak -->
+            <div class="col-md-4" style="padding: 20px;">
+                <h4 style="color: #00aaff; margin-bottom: 15px;">Pencarian & Kontak</h4>
+                <form action="#" method="get" style="margin-bottom: 15px;">
+                    <input type="text" class="form-control" placeholder="Cari sesuatu...">
+                </form>
+                <p><i class="glyphicon glyphicon-map-marker"></i> Jl. Limbangan No.51381, Salamsari</p>
+                <p><i class="glyphicon glyphicon-envelope"></i> contact@medialaundry.com</p>
+                <p><i class="glyphicon glyphicon-earphone"></i> +62 882-2683-7445</p>
+            </div>
+
+            <!-- Jam Operasional -->
+            <div class="col-md-4" style="padding: 20px;">
+                <h4 style="color: #00aaff; margin-bottom: 15px;">Jam Operasional</h4>
+                <p>Senin - Kamis : 8.00 - 21.00</p>
+                <p>Jumat - Sabtu : 8.00 - 01.00</p>
+                <p>Minggu : 9.00 - 22.00</p>
+            </div>
+
+        </div>
+
+        <div class="text-center" style="margin-top: 30px; padding-top: 15px; border-top: 1px solid #333; color: #bbb;">
+            &copy; 2025 <b>media Laundry</b>. All Rights Reserved.
+        </div>
+    </div>
+</footer>
